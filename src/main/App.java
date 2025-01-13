ea6b80e653314197edbe297d0cf30eb8e638d033
eliaslehtinen <elias.4664@hotmail.com>
2025-01-13 15:51:13 +0200

    first commit

---
+++ src/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+import main.Animal;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Pikseli");
+        System.out.println("Hedgehog");
+        System.out.println(13);
+    }
+}
